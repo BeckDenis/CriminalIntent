7be1f24900b2774582391a1912a9fc1b6a1bbd51
Denis <denisbeck555@gmail.com>
2020-01-06 12:01:53 +0600

    added room

--- app/src/main/java/com/example/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/android/criminalintent/CrimeListFragment.kt
@@ -8,9 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_crime_list.*
 import kotlinx.android.synthetic.main.fragment_crime_list.view.*
 import kotlinx.android.synthetic.main.list_item_crime.view.*
 import java.text.DateFormat
@@ -19,15 +21,10 @@ private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
 
-    private var adapter: CrimeAdapter? = null
+    private var crimeAdapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
     }
 
     companion object {
@@ -42,20 +39,34 @@ class CrimeListFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
 
-        view.crime_recycler_view.layoutManager = LinearLayoutManager(context)
-
-        updateUI(view)
+        view.crime_recycler_view.run {
+            layoutManager = LinearLayoutManager(context)
+            adapter = crimeAdapter
+        }
 
         return view
     }
 
-    private fun updateUI(view: View) {
-        val crimes = crimeListViewModel.crimes
-        adapter = CrimeAdapter(crimes)
-        view.crime_recycler_view.adapter = adapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            })
+
     }
 
-    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+    private fun updateUI(crimes: List<Crime>) {
+        crimeAdapter = CrimeAdapter(crimes)
+        crime_recycler_view.adapter = crimeAdapter
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
         private lateinit var crime: Crime
 
         init {
@@ -65,7 +76,8 @@ class CrimeListFragment : Fragment() {
         fun bind(crime: Crime) {
             this.crime = crime
             itemView.crime_title.text = crime.title
-            itemView.crime_date.text = DateFormat.getDateInstance(DateFormat.FULL).format(crime.date)
+            itemView.crime_date.text =
+                DateFormat.getDateInstance(DateFormat.FULL).format(crime.date)
 
             itemView.crime_solved.visibility = if (crime.isSolved) {
                 View.VISIBLE
@@ -80,13 +92,14 @@ class CrimeListFragment : Fragment() {
         }
     }
 
-    private inner class CrimeAdapter(var crimes: List<Crime>)
-        : RecyclerView.Adapter<CrimeHolder>() {
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
                 : CrimeHolder {
             val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
             return CrimeHolder(view)
         }
+
         override fun getItemCount() = crimes.size
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
--- app/src/main/java/com/example/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/android/criminalintent/CrimeListViewModel.kt
@@ -3,11 +3,6 @@ package com.example.android.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
-
-    init {
-        for (i in 0..99) {
-            crimes += Crime(title = "Crime: #$i", isSolved = i % 2 == 0)
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
 }
